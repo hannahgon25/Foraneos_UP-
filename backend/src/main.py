70dedd82385da75ca5e329f7048c57a944f69cb8
diego <diegoschacht25@gmail.com>
2025-11-07 13:01:40 -0600

    Ya casi

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+import mysql.connector
+from contextlib import asynccontextmanager
+import time
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+	try:
+		db = mysql.connector.connect(
+			host="mysql",
+			port=3306,
+			user="root",
+			password="root",
+			database="foraneos"
+		)
+		print("\tHola")
+	except mysql.connector.Error as err:
+		print(f"Erorr: {err}")
+	yield
+	print("\tadios papus :'v")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
